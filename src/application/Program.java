94f2bcd2af0099663dc11436f7cc58b5f71a40fd
Janiel Marques <janielmarques@icloud.com>
2025-02-14 20:56:30 -0300

    breve introducao a Tipos Curingas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import services.CalculationService;
 import services.PrintService;
 
@@ -8,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -15,27 +15,15 @@ import java.util.Scanner;
 import entities.Product;
 
 public class Program {
+
     public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+    }
 
-		List<Product> list = new ArrayList<>();
-        String path = "/home/janiel-marques/Downloads/5 10 7 14 8.txt";
-		
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			String line = br.readLine();
-			while (line != null) {
-                String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-                line = br.readLine();
-			}
-			
-			Product x = CalculationService.max(list);
-			System.out.println("Max: ");
-			System.out.println(x);
-
-		} catch (IOException e) {
-		System.out.println("Error: " + e.getMessage());
-		}
-	}
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
 }
