c7f10462d2f44d359d4a4dab5ac60b1a3bb5809c
Janiel Marques <janielmarques@icloud.com>
2025-02-14 20:41:23 -0300

    aula e exemplos genericos delimitados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,41 @@
 package application;
 
 
+import services.CalculationService;
 import services.PrintService;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+        String path = "/home/janiel-marques/Downloads/5 10 7 14 8.txt";
+		
 
-        PrintService<String> ps = new PrintService<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-        System.out.print("How many values? ");
-        int n = sc.nextInt();
+			String line = br.readLine();
+			while (line != null) {
+                String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Max: ");
+			System.out.println(x);
 
-
-        for (int i = 0; i < n; i++) {
-            String value = sc.next();
-            ps.addValue(value);
-        }
-        ps.print();
-        String x = ps.first();
-        System.out.println("First: "  + x);
-
-        sc.close();
-    }
+		} catch (IOException e) {
+		System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
